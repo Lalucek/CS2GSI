b68e806ec25a7493b7c7aef1ff5857a8da713607
Jan Mrzílek <mrzilja3@fd.cvut.cz>
2025-11-22 16:49:34 +0100

    upadted setup to use requirements file

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="cs2gsi",
     version="1.0.0",
     packages=find_packages(),
-    install_requires=[
-        "aiohttp>=3.8.0",
-    ],
+    install_requires=requirements,
     author="Jan Mrzílek",
     description="CS2 Game State Integration Python reader",
     url="https://github.com/Lalucek/CS2GSI",
